153a87c6bb1b9e1ee7600e1379f6cb29f21954ea
David Ricardo Aristizábal Ramírez <draristizabalr@unal.edu.co>
2022-08-13 17:37:45 -0500

    Creando metodo para los botones de seleccionar carpeta

--- Ventana.py
+++ Ventana.py
@@ -15,55 +15,67 @@ class Window(QMainWindow):
         layout = QGridLayout()
 
         # Creamos objeto lector de archivos
-        self.modelo = QFileSystemModel()
-        self.modelo.setRootPath('')
+        self.modelo_buscar = QFileSystemModel()
+        self.modelo_buscar.setRootPath('')
+        self.modelo_guardar = QFileSystemModel()
+        self.modelo_guardar.setRootPath('')
 
         # Creamos los árboles de directorios
-        self.explorador_analizar = QTreeView()
-        self.explorador_analizar.setModel(self.modelo)
-        self.explorador_analizar.setIndentation(20)
-        self.explorador_analizar.setAnimated(False)
-        self.explorador_analizar.setStyleSheet('QTreeView{font-size: 13pt;}')
-        self.explorador_analizar.setSortingEnabled(False)
-        self.explorador_analizar.setColumnWidth(0, 500)
-        self.explorador_entrega = QTreeView()
-        self.explorador_entrega.setModel(self.modelo)
-        self.explorador_entrega.setIndentation(20)
-        self.explorador_entrega.setAnimated(False)
-        self.explorador_entrega.setStyleSheet('QTreeView{font-size: 13pt;}')
-        self.explorador_entrega.setSortingEnabled(False)
-        self.explorador_entrega.setColumnWidth(0, 500)
+        self.explorador_buscar = QTreeView()
+        self.explorador_buscar.setModel(self.modelo_buscar)
+        self.explorador_buscar.setIndentation(20)
+        self.explorador_buscar.setAnimated(False)
+        self.explorador_buscar.setStyleSheet('QTreeView{font-size: 13pt;}')
+        self.explorador_buscar.setSortingEnabled(False)
+        self.explorador_buscar.setColumnWidth(0, 500)
 
+        self.explorador_guardar = QTreeView()
+        self.explorador_guardar.setModel(self.modelo_guardar)
+        self.explorador_guardar.setIndentation(20)
+        self.explorador_guardar.setAnimated(False)
+        self.explorador_guardar.setStyleSheet('QTreeView{font-size: 13pt;}')
+        self.explorador_guardar.setSortingEnabled(False)
+        self.explorador_guardar.setColumnWidth(0, 500)
+
+        # Agregamos una caja de texto donde agregar el nombre del archivo
         self.nombre_archivo = QLineEdit()
 
         # Creamos los botones
-        boton_carpeta_analizar = QPushButton('Seleccionar carpeta')
-        boton_carpeta_entrega = QPushButton('Seleccionar carpeta')
-        boton_una_carpeta = QPushButton('Carpeta principal')
-        boton_una_carpeta.clicked.connect(self.analisis_una_carpeta)
-        boton_todas_carpetas = QPushButton('Carpeta principal y subcarpetas')
+        boton_buscar = QPushButton('Seleccionar carpeta')
+        boton_buscar.setStyleSheet('QPushButton{font-size: 14pt;}')
+        boton_buscar.clicked.connect(self.imprimir_direccion)
 
+        boton_guardar = QPushButton('Seleccionar carpeta')
+        boton_guardar.setStyleSheet('QPushButton{font-size: 14pt;}')
+
+        boton_una_carpeta = QPushButton('Carpeta principal')
+        boton_una_carpeta.setStyleSheet('QPushButton{font-size: 16pt;}')
+        boton_una_carpeta.clicked.connect(self.analisis_una_carpeta)
+
+        boton_todas_carpetas = QPushButton('Carpeta principal y subcarpetas')
+        boton_todas_carpetas.setStyleSheet('QPushButton{font-size: 16pt;}')
 
         # Creamos las etiquetas
         etiqueta_analizar = QLabel('Buscar en:')
         etiqueta_analizar.setStyleSheet('QLabel{font-size: 20pt;}')
         etiqueta_destino = QLabel('Guardar en:')
         etiqueta_destino.setStyleSheet('QLabel{font-size: 20pt;}')
-        etiqueta_nombre_archivo = QLabel('Nombre del archivo que se va a generar:')
+        # etiqueta_nombre_archivo = QLabel('Nombre del archivo que se va a generar:')
+        # etiqueta_nombre_archivo.setStyleSheet('QLabel{font-size: 15pt;}')
 
         # Agregamos los botones al layout
-        layout.addWidget(boton_carpeta_analizar, 2, 2)
-        layout.addWidget(boton_carpeta_entrega, 4, 2)
-        layout.addWidget(boton_una_carpeta, 5, 2)
-        layout.addWidget(boton_todas_carpetas, 6, 2)
+        layout.addWidget(boton_buscar, 2, 2)
+        layout.addWidget(boton_guardar, 4, 2)
+        layout.addWidget(boton_una_carpeta, 5, 1)
+        layout.addWidget(boton_todas_carpetas, 5, 2)
 
         # Agregamos los otros objetos al layout
         layout.addWidget(etiqueta_analizar, 1, 1)
-        layout.addWidget(self.explorador_analizar, 2, 1)
+        layout.addWidget(self.explorador_buscar, 2, 1)
         layout.addWidget(etiqueta_destino, 3, 1)
-        layout.addWidget(self.explorador_entrega, 4, 1)
-        layout.addWidget(etiqueta_nombre_archivo, 5, 1)
-        layout.addWidget(self.nombre_archivo, 6, 1)
+        layout.addWidget(self.explorador_guardar, 4, 1)
+        # layout.addWidget(etiqueta_nombre_archivo, 5, 1)
+        # layout.addWidget(self.nombre_archivo, 6, 1)
 
         # Creamos un componente genérico para publicar
         componente = QWidget()
@@ -80,6 +92,10 @@ class Window(QMainWindow):
         direccion_entrega = direccion_entrega.replace('\\', '/')
         df = Exportador(direccion_analisis)
         df.exportar_excel(f'{direccion_entrega}/{nombre_archivo}.xlsx')
+
+    def imprimir_direccion(self):
+        print('Está conectado')
+        print(self.modelo_buscar.rootPath())
 
 
 if __name__ == '__main__':
